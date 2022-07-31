3ada3d294259e8c251944d20c20784e1d16bd507
bhuwan55 <awsumbj55@gmail.com>
2022-07-31 21:49:11 +0545

    added get edge list operations

--- graph/graph.py
+++ graph/graph.py
@@ -41,7 +41,10 @@ class Graph(object):
         """Don't return a list of edge objects!
         Return a list of triples that looks like this:
         (Edge Value, From Node Value, To Node Value)"""
-        return []
+        edge_list = []
+        for edge in self.edges:
+            edge_list.append((edge.value,edge.node_from.value,edge.node_to.value))
+        return edge_list
 
     def get_adjacency_list(self):
         """Don't return any Node or Edge objects!
